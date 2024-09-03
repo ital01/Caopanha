25fff57831b65f9bf4ae411b73274ce8b71a473f
italoGlobals <italo.almeida@weareglobals.com>
2024-09-03 16:52:49 -0300

    styles: index css

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-// main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Home } from './app/screens/home/home';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </StrictMode>,
 );
