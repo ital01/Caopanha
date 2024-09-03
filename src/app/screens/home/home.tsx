e3e4cff25534d1ccb8436daef765cae1e68bcef0
italoGlobals <italo.almeida@weareglobals.com>
2024-09-03 16:43:48 -0300

    feat: routes

---
+++ src/app/screens/home/home.tsx
@@ -0,0 +1,10 @@
+import { ViewUsers } from "@components/users/users";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ViewUsers />
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Outlet } from 'react-router-dom';
-import './index.css'
+// main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Home } from './app/screens/home/home';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Outlet />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   </StrictMode>,
-)
+);
