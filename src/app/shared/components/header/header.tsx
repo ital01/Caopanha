4f0234dbd6f8fa3e92a7572433a806c44460f892
italo <italolenza@gmail.com>
2024-09-03 20:48:05 -0300

    feat: header

--- src/app/shared/components/header/header.tsx
+++ src/app/shared/components/header/header.tsx
@@ -1,12 +1,56 @@
+const isLogged = true;
+
 const Header = () => {
   return (
-    <header className='flex-row'>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+    <header 
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#79B8AA',
+        padding: '1.5rem 3rem',
+        fontWeight: 'bold',
+        fontSize: '1.8rem'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: '1.6rem'
+        }}
+      >
+        <img 
+          src="svg/brasao-americana.svg" 
+          height={'60px'}
+          width={'auto'}
+          alt="Brasão de Americana" 
+        />
+        <p>Prefeitura de Americana</p>
+      </div>
+
+      <nav
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'end',
+          gap: '4rem'
+        }}
+      >
+        <a href="/">Como Funciona ?</a>
+        <a href="/">Campanhas Ativas</a>
+        {
+          isLogged ? (
+            <>
+              <a href="/">Agendamentos</a>
+              <a href="/">Meus Pets</a>
+              <a href="/">Minha Conta</a>
+            </>
+          ) : (
+            <a href="/">Fazer Login</a>
+          )
+        }
       </nav>
     </header>
   );
