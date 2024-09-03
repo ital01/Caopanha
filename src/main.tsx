eecdb8f2922ca42f4b628078436f13ff2e3bb420
italoGlobals <italo.almeida@weareglobals.com>
2024-09-03 16:24:05 -0300

    feat: icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { Outlet } from 'react-router-dom';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Outlet />
   </StrictMode>,
 )
