1a794d068d8de603481acd52fb1f048bbd98544c
italo <italolenza@gmail.com>
2024-09-04 08:59:48 -0300

    feat: home image and title

--- src/app/screens/home/home.tsx
+++ src/app/screens/home/home.tsx
@@ -1,10 +1,23 @@
-import { ViewUsers } from "@components/users/users";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <ViewUsers />
-    </div>
+    <main
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '2rem auto'
+      }}
+    >
+      <h1>
+        <span>Agora ficou fácil cuidar do seu </span>
+        <span style={{color: "#01BBB2"}}>a</span>
+        <span style={{color: "#FE684D"}}>m</span>
+        <span style={{color: "#01BBB2"}}>i</span>
+        <span style={{color: "#FE684D"}}>g</span>
+        <span style={{color: "#01BBB2"}}>o</span>
+      </h1>
+      <h2>Agende por aqui a vacinação do seu pet</h2>
+      <img src="images/pets.png" alt="" />
+    </main>
   );
 };
